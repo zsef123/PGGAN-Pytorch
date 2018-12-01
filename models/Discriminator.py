683a260409bddf21d189cf6d8baa188439a34168
AhnDW <zsef123@gmail.com>
2018-12-01 23:25:38 +0900

    New network structure

--- models/Discriminator.py
+++ models/Discriminator.py
@@ -6,22 +6,23 @@ from models.Minibatch_stddev import MiniBatchSTD
 from models.EqualizedLR import EqualizedLRLayer
 from config import resl_to_ch
 
+
 class FromRGBLayer(nn.Module):
     def __init__(self, resl):
         super().__init__()
+
         _, out_c = resl_to_ch[resl]
         self.conv = EqualizedLRLayer(nn.Conv2d(3, out_c, 1, bias=False))
 
     def forward(self, x):
         return self.conv(x)
 
-class _DownReslBlock(nn.Module):
-    def __init__(self, resl):
+
+class DownReslLayer(nn.Module):
+    def __init__(self, resl, second_conv_kernel=3):
         super().__init__()
-        self.resl = resl
-        # in_c, out_c is define by resl
+
         out_c, in_c  = resl_to_ch[resl]
-        
         self.conv = nn.Sequential(
             EqualizedLRLayer(nn.Conv2d(in_c,  out_c, 3, 1, 1, bias=False)),
             nn.LeakyReLU(inplace=True),
@@ -29,10 +30,28 @@ class _DownReslBlock(nn.Module):
             nn.LeakyReLU(inplace=True),
             nn.AvgPool2d(2)
         )
-        
     
-    def forward(self, x):        
+    def forward(self, x):
         return self.conv(x)
+
+class DownReslBlock(nn.Module):
+    def __init__(self, resl, grop_size):
+        super().__init__()
+
+        self.layers = nn.Sequential()
+        self.layers.add_module("miniBatchSTD", MiniBatchSTD(group_size=group_size))
+        self.layers.add_module("%d_DownReslLayer"%(resl), DownReslLayer(self.resl, second_conv_kernel=4))
+        self.first_layer = None
+
+    def grow_network(self, resl):
+        key = "%d_DownReslLayer"%(resl)
+        self.layers.add_module(key, self.first_layer(self.resl))
+        self.layers._modules.move_to_end(key, last=False)
+        self.first_layer = DownReslLayer(resl)
+    
+    def forward(self, x):
+        return self.layers(x)
+
 
 class D(nn.Module):
     def __init__(self, resl=4, group_size=1):
@@ -40,15 +59,7 @@ class D(nn.Module):
         self.resl = resl
 
         in_c, out_c = resl_to_ch[resl]
-        self.resl_blocks = nn.Sequential(nn.Sequential(
-            MiniBatchSTD(group_size=group_size),
-            EqualizedLRLayer(nn.Conv2d(in_c + 1, out_c, 3, 1, 1, bias=False)),
-            nn.LeakyReLU(inplace=True),
-            EqualizedLRLayer(nn.Conv2d(in_c, out_c, 4, 1, 1, bias=False)),
-            nn.LeakyReLU(inplace=True),
-            nn.AdaptiveAvgPool2d(out_c),
-            EqualizedLRLayer(nn.Linear(out_c, 1, bias=False))
-        ))
+        self.resl_blocks = DownReslBlock(resl, group_size)
 
         self.rgb_l = None
         self.rgb_h = FromRGBLayer(resl)
@@ -62,7 +73,7 @@ class D(nn.Module):
 
     def grow_network(self):
         self.resl *= 2
-        self.resl_blocks = nn.Sequential([_DownReslBlock(self.resl), *self.resl_blocks])
+        self.resl_blocks.grow_network(self.resl)
         self.rgb_l = self.rgb_h
         self.rgb_h = FromRGBLayer(self.resl)
         self.alpha = 0
@@ -74,16 +85,13 @@ class D(nn.Module):
 
         # high resolution path
         rgb_h = self.rgb_h(x)
-        x_high = self.resl_blocks[0](rgb_h)
+        x_high = self.resl_blocks.first_layer(rgb_h)
 
         x = (self.alpha * x_high) + ((1 - self.alpha) * rgb_l)
-
-        for resl in self.resl_blocks[1:]:
-            x = resl(x)
+        x = self.resl_blocks(x)        
         return x
 
     def stabilization_forward(self, x):
         x = self.rgb_h(x)
-        for resl in self.resl_blocks:
-            x = resl(x)
+        x = self.resl_blocks(x)
         return x
--- models/Generator.py
+++ models/Generator.py
@@ -5,29 +5,28 @@ from models.PixelWiseNorm import PixelWiseNormLayer
 from models.EqualizedLR import EqualizedLRLayer
 from config import resl_to_ch
 
+
 class ToRGBLayer(nn.Module):
     def __init__(self, resl):
         super().__init__()
-        _, in_c  = resl_to_ch[resl]
 
+        _, in_c  = resl_to_ch[resl]
         self.conv = nn.Sequential(
             EqualizedLRLayer(nn.Conv2d(in_c, 3, 1, bias=False)),
+            nn.Tanh()
         )
-        self.resl = resl
 
     def forward(self, x):
         return self.conv(x)
 
 
-class ReslBlock(nn.Module):
-    def __init__(self, resl):
+class ReslLayer(nn.Module):
+    def __init__(self, resl, first_conv_kernel=3):
         super().__init__()
-        self.resl = resl
-        in_c, out_c  = resl_to_ch[resl]        
-        # print("UpResl resl : ", resl, "in_c : ", in_c, "out_c :", out_c)
 
+        in_c, out_c  = resl_to_ch[resl]        
         self.conv = nn.Sequential(
-            EqualizedLRLayer(nn.Conv2d(in_c, out_c,  3, 1, 1, bias=False)),
+            EqualizedLRLayer(nn.Conv2d(in_c, out_c, first_conv_kernel, 1, 1, bias=False)),
             PixelWiseNormLayer(),
             nn.LeakyReLU(inplace=True),
             EqualizedLRLayer(nn.Conv2d(out_c, out_c, 3, 1, 1, bias=False)),
@@ -40,25 +39,42 @@ class ReslBlock(nn.Module):
         conv = self.conv(up)
         return conv
 
+class ReslBlock(nn.Module):
+    def __init__(self, resl):
+        super().__init__()
+        
+        self.layers = nn.Sequential()
+        self.layers.add_module("%d_ReslLayer"%(resl), ReslLayer(self.resl, first_conv_kernel=4))
+        self.last_layer = None
+
+    def grow_network(self, resl):
+        self.layers.add_module("%d_ReslLayer"%(resl), self.last_layer)
+        self.last_layer = ReslLayer(resl)
+    
+    def transition_forward(self, x):
+        x_low = self.layers(x)
+        x_high = self.last_layer(x_low)
+        return x_low, x_high
+    
+    def stabilization_forward(self, x):
+        x = self.layers(x)
+        return x
+
+
 class G(nn.Module):
-    def __init__(self, resl=4):
+    def __init__(self, resl=4, device=None):
         super().__init__()
         self.resl = resl
 
         in_c, out_c = resl_to_ch[resl]
-        self.resl_blocks = nn.Sequential(nn.Sequential(
-            EqualizedLRLayer(nn.ConvTranspose2d(in_c, out_c, 4, bias=False)),
-            PixelWiseNormLayer(),
-            nn.LeakyReLU(inplace=True),
-            EqualizedLRLayer(nn.Conv2d(out_c, out_c, 3, 1, 1, bias=False)),
-            PixelWiseNormLayer(),
-            nn.LeakyReLU(inplace=True),
-        ))
+        self.resl_block = ReslBlock(resl)
 
         self.rgb_l = None
         self.rgb_h = ToRGBLayer(self.resl)
 
         self.alpha = 0
+
+        self.device = device
         
     def forward(self, x, phase):
         if phase == "transition":
@@ -68,27 +84,24 @@ class G(nn.Module):
 
     def grow_network(self):
         self.resl *= 2
-        self.resl_blocks.add_module("%d_ReslBlock"%self.resl, ReslBlock(self.resl))        
+        self.resl_block.grow_network(self.resl)
         self.rgb_l = self.rgb_h
         self.rgb_h = ToRGBLayer(self.resl)
         self.alpha = 0
 
-    def transition_forward(self, x):        
-        for resl in self.resl_blocks[:-1]:
-            x = resl(x)
+    def transition_forward(self, x):
+        x_low, x_high = self.resl_block.transition_forward(x)
 
         # low resolution path
-        x_up = F.interpolate(x, scale_factor=2)        
+        x_up = F.interpolate(x_low, scale_factor=2)        
         rgb_l = self.rgb_l(x_up)
 
         # high resolution path
-        x = self.resl_blocks[-1](x)
-        rgb_h = self.rgb_h(x)
+        rgb_h = self.rgb_h(x_high)
         return (self.alpha * rgb_h) + ((1 - self.alpha) * rgb_l)
 
     def stabilization_forward(self, x):
-        for resl in self.resl_blocks:
-            x = resl(x)
+        x = self.resl_block(x)
         rgb_h = self.rgb_h(x)
         return rgb_h
 
